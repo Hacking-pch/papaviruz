25e8dd7324550892e1ecb2369a2fd1442341ea16
Hacking-pch <58540697+Hacking-pch@users.noreply.github.com>
2020-02-25 09:17:33 -0600

    Add files via upload

---
+++ .WINDOWNS.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+#
+# Obfuscate by @CesarHackGray
+# Creditos para @CesarHackGray
+# Servicio de ayuda https://t.me/CesarGray
+# Channel: https://www.youtube.com/channel/UCjs0N8PbEo-se0r_4O_svNQ
+#
+#
+z="
+";Dz=' " "';Rz=' por';qz='bash';Xz='le e';Tz='no e';Iz='El s';mz='RONT';cz='eral';Qz='ones';Pz='zaci';Cz='echo';Mz=' sus';Wz='onib';hz='pv -';lz='UY P';Yz='opci';Vz='disp';Hz='[36m';uz='h';dz='o mu';Jz='crip';Gz='1m\e';Fz='"\e[';pz='p 3';Oz='uali';jz='0;';Sz='eso ';Uz='sta ';Bz='r';oz='slee';Ez=' -e ';tz='uz.s';Az='clea';Zz=' asi';kz='31mM';Kz='t te';iz='qL 1';Nz=' act';Lz='ndra';rz=' pap';ez='y pr';sz='avir';bz=' esp';gz='." |';nz='O" |';az=' que';fz='onto';
+eval "$Az$Bz$z$Cz$Dz$z$Cz$Dz$z$Cz$Dz$z$Cz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Uz$Yz$Qz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$z$Cz$Dz$z$Cz$Ez$Fz$kz$lz$mz$nz$hz$iz$jz$z$oz$pz$z$qz$rz$sz$tz$uz"
